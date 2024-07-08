491a23be5075d113fbab87f8ce4ae7ba6e973845
Junseo <50725139+kojunseo@users.noreply.github.com>
2024-07-08 11:29:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='SumRAG',
-    version='0.1.2',
+    version='0.1.3',
     author="Junseo Ko",
     description="RAG with summarization and LLM based retrieve for chapter-based documents",
     packages=find_packages(include=['SumRAG', 'SumRAG.*']),
